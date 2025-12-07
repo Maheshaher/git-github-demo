9b7c978a57f94e56c3df86896b0583428f31e31b
mahii <maheshaher546@gmail.com>
2025-12-07 12:47:23 +0530

    new project

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static int add(int a ,int b){
+        return a+b;
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int Add=add(4,9);
+        System.out.println(Add);
+
+
+
+
+        }
+    }
